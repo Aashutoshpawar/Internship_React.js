5c8ba262aa222dd85559fb02946904c357133daf
Aashutosh Rajendra Pawar <aashutoshpawar123@gmail.com>
2024-03-18 22:06:28 +0530

    Add files via upload

---
+++ Import-Export/import_Export/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import {Title} from "./Title";
+
+
+
+function App(){
+  return(
+    <div>
+
+      <h1>hello this is aashutosh</h1>
+      <Title/>
+      <h3>the sum of 2 + 2 is {2+2}</h3>
+      <button>click here</button>
+    </div>
+  );
+}
+export default App;
---
+++ Import-Export/import_Export/src/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+
+function Title(){
+    return(
+        <div>
+            <h3>Hello I am aashutosh pawar i am a MCA 1st year student from suryadatta institute of business management and technology</h3>
+            <marquee behavior="" direction="">THANK YOU </marquee>
+        </div>
+    );
+
+}
+export {Title};
